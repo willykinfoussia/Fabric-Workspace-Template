4804afe69254256649afa4549d8d4fa6556aa193
Kinfoussia Willy <41423122+willykinfoussia@users.noreply.github.com>
2026-04-17 09:34:54 +0200

    Committing 8 items from workspace 74c649b9-b15e-41ec-8695-d13d59eef281

---
+++ GOLD/SILVER_TO_GOLD.Notebook/notebook-content.py
@@ -0,0 +1,41 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "c46296fa-e5ae-4a72-bf4b-e745471968f8",
+# META       "default_lakehouse_name": "LH_SILVER",
+# META       "default_lakehouse_workspace_id": "74c649b9-b15e-41ec-8695-d13d59eef281",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "c46296fa-e5ae-4a72-bf4b-e745471968f8"
+# META         },
+# META         {
+# META           "id": "2b78a9e8-9637-4414-ae85-0c8814a4f3c9"
+# META         }
+# META       ]
+# META     },
+# META     "environment": {
+# META       "environmentId": "42851e6f-dd2a-472f-87e9-ba257a479276",
+# META       "workspaceId": "e0935465-85f7-4785-934c-b3e333131f66"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
---
+++ SILVER/BRONZE_TO_SILVER.Notebook/notebook-content.py
@@ -0,0 +1,41 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "867e0bad-c5ea-44a4-81d6-6829e2486a52",
+# META       "default_lakehouse_name": "LH_BRONZE",
+# META       "default_lakehouse_workspace_id": "74c649b9-b15e-41ec-8695-d13d59eef281",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "867e0bad-c5ea-44a4-81d6-6829e2486a52"
+# META         },
+# META         {
+# META           "id": "c46296fa-e5ae-4a72-bf4b-e745471968f8"
+# META         }
+# META       ]
+# META     },
+# META     "environment": {
+# META       "environmentId": "42851e6f-dd2a-472f-87e9-ba257a479276",
+# META       "workspaceId": "e0935465-85f7-4785-934c-b3e333131f66"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
